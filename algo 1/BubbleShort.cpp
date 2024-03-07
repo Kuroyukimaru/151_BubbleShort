c92324b879d07a552974be65e62d6222aa5de03b
Kuroyukimaru <oranggabut712@gmail.com>
2024-03-07 13:44:00 +0700

    mengubah pass + 1 ke pass ++

--- algo 1/BubbleShort.cpp
+++ algo 1/BubbleShort.cpp
@@ -40,7 +40,7 @@ void display() {				// Prosedur untuk menampilkan hasil
 void bubbleSortArray() {								// Looping dengan i dimulai dari 1 hingga n-1
 	int pass = 1;										// Step 1
 
-	for (pass; pass <= n - 1; pass + 1) {				// looping dengan i dimulai dari 1 hingga n-1
+	for (pass; pass <= n - 1; pass++) {				// looping dengan i dimulai dari 1 hingga n-1
 		for (int j = 0; j <= n - 1 - pass; j++) {		// Looping dengan j dimulai dari 0 hingga n-1
 			if (a[j] > a[j + 1]) {							// Jika nilai pada a[j] lebih besar dari a[j+1]
 				int temp = a[j];							// Simpan nilai a[j] ke variabel sementara temp
