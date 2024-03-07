37ba9a1f5478f3161d6f829f1685b0c4fc58e059
Kuroyukimaru <oranggabut712@gmail.com>
2024-03-07 11:49:09 +0700

    Menyelesaikan prosedur bubblesort dan memanggil semua prosedur di main

--- algo 1/BubbleShort.cpp
+++ algo 1/BubbleShort.cpp
@@ -33,4 +33,27 @@ void display() {				// Prosedur untuk menampilkan hasil
 	for (int j = 0; j < n; j++) {						// Looping dengan j dimulai dari 0 hingga n-1
 		cout << "Data ke-" << j + 1 << ": ";
 		cout << a[j] << endl;							// Output ke layar
+	}
+	cout << endl;										// Output baris kosong
+}
+
+void bubbleSortArray() {								// Looping dengan i dimulai dari 1 hingga n-1
+	int pass = 1;										// Step 1
+
+	for (pass; pass <= n - 1; pass + 1) {				// looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j <= n - 1 - pass; j++) {		// Looping dengan j dimulai dari 0 hingga n-1
+			if (a[j] > a[j + 1]) {							// Jika nilai pada a[j] lebih besar dari a[j+1]
+				int temp = a[j];							// Simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];							// Assign nilai a[j+1] ke a[j]
+				a[j + 1] = temp;							// Assign nilai temp ke a[j+1]
+			}
+		}
+	}
+}
+
+int main() {
+	input();			//Memanggil read()
+	bubbleSortArray();	// Memanggil bubblesortarray ()
+	display();			// Memanggil dsisplay ()
+	return 0;
 }
